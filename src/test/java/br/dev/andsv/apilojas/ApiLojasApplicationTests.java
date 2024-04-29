1168a877136551b3809ceb2a2085c8d38de6f6bb
Anderson S.  Vieira <svieira.anderson@gmail.com>
2024-04-29 16:40:16 -0300

    test: Cria teste para verificar endpoint GET para LojaFisica

--- src/test/java/br/dev/andsv/apilojas/ApiLojasApplicationTests.java
+++ src/test/java/br/dev/andsv/apilojas/ApiLojasApplicationTests.java
@@ -1,13 +1,30 @@
 package br.dev.andsv.apilojas;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApiLojasApplicationTests {
+
+    @Autowired
+    TestRestTemplate restTemplate;
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void shouldReturnAPhysicalStoreWhenDataIsSaved() {
+        ResponseEntity<String> response = restTemplate
+                .getForEntity("/fisica/99", String.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 
 }
