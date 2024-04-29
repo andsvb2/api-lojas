6d6077c9c6f70f6070eda23f4e2f688df24e4bd0
Anderson S.  Vieira <svieira.anderson@gmail.com>
2024-04-29 20:06:36 -0300

    test: Cria teste para verificar o endpoint de GET

--- src/test/java/br/dev/andsv/apilojas/ApiLojasApplicationTests.java
+++ src/test/java/br/dev/andsv/apilojas/ApiLojasApplicationTests.java
@@ -37,6 +37,24 @@ class ApiLojasApplicationTests {
                 .getForEntity("/fisica/99", String.class);
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        Number id = documentContext.read("$.id");
+        assertThat(id).isEqualTo(99);
+
+        String cnpj = documentContext.read("$.cnpj");
+        assertThat(cnpj).isEqualTo("15.916.727/0001-90");
+
+        String nome = documentContext.read("$.nome");
+        assertThat(nome).isEqualTo("Outlet Express");
+
+        Number enderecoId = documentContext.read("$.endereco.id");
+        assertThat(enderecoId).isEqualTo(207);
+
+        String logradouro = documentContext.read("$.endereco.logradouro");
+        assertThat(logradouro).isEqualTo("Rua Trigésima");
+
+        System.out.println(documentContext);
     }
 
 }
