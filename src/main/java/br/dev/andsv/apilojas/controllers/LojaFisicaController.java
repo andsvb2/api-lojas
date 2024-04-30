d4570bcd78fe52db6a57c786cb1b0468db5d2f5f
Anderson S.  Vieira <svieira.anderson@gmail.com>
2024-04-30 10:05:39 -0300

    feat: Implementa criação de LojaFisica

--- src/main/java/br/dev/andsv/apilojas/controllers/LojaFisicaController.java
+++ src/main/java/br/dev/andsv/apilojas/controllers/LojaFisicaController.java
@@ -3,11 +3,10 @@ package br.dev.andsv.apilojas.controllers;
 import br.dev.andsv.apilojas.core.entities.LojaFisica;
 import br.dev.andsv.apilojas.repository.LojaFisicaRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Optional;
 
 /**
@@ -37,6 +36,18 @@ public class LojaFisicaController {
             return ResponseEntity.ok(lojaFisica.get());
         }
         return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    private ResponseEntity<Void> criarLojaFisica(
+            @RequestBody LojaFisica novaLojaFisica,
+            UriComponentsBuilder ucb) {
+        LojaFisica lojaFisicaSalva = lojaFisicaRepository.save(novaLojaFisica);
+        URI localdaNovaLojaFisica = ucb
+                .path("fisica/{id}")
+                .buildAndExpand(lojaFisicaSalva.getId())
+                .toUri();
+        return ResponseEntity.created(localdaNovaLojaFisica).build();
     }
 
 }
