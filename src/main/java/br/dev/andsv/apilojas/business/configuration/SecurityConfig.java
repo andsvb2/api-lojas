0676c13d99835498fcb3ba644c3a3116dfd21782
Anderson S.  Vieira <svieira.anderson@gmail.com>
2024-05-01 11:08:05 -0300

    feat: configura autorização de acesso baseado em roles

--- src/main/java/br/dev/andsv/apilojas/business/configuration/SecurityConfig.java
+++ src/main/java/br/dev/andsv/apilojas/business/configuration/SecurityConfig.java
@@ -2,9 +2,14 @@ package br.dev.andsv.apilojas.business.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,11 +17,33 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/api/v1/**")
+                        .authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable());
         return http.build();
     }
 
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    UserDetailsService testOnlyUsers(PasswordEncoder passwordEncoder) {
+        User.UserBuilder users = User.builder();
+        UserDetails andsv = users
+                .username("andsvb2")
+                .password(passwordEncoder.encode("abc123"))
+                .roles("RESPONSAVEL")
+                .build();
+        UserDetails tmpNaoEResponsavel = users
+                .username("tmp-nao-e-responsavel")
+                .password(passwordEncoder.encode("qrs456"))
+                .roles("NAO-RESPONSAVEL")
+                .build();
+        return new InMemoryUserDetailsManager(andsv, tmpNaoEResponsavel);
     }
 }
